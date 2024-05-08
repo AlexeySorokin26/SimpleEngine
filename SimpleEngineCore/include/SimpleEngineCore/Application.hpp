296ab0ae0c86056f3a9e585a5c74708b159e8110
Alexey Sorokin <asorokin@gremse-it.com>
2024-05-08 23:30:33 +0300

    refactoring

--- SimpleEngineCore/include/SimpleEngineCore/Application.hpp
+++ SimpleEngineCore/include/SimpleEngineCore/Application.hpp
@@ -1,32 +1,40 @@
 #pragma once
 
 #include "SimpleEngineCore/Event.hpp"
+#include "SimpleEngineCore/Camera.hpp"
 
 #include <memory>
 
-namespace SimpleEngine{
+namespace SimpleEngine {
 
-class Window;
-
-    class Application{
+    class Application
+    {
     public:
         Application();
-
         virtual ~Application();
-        
+
         Application(const Application&) = delete;
-        Application(const Application&&) = delete;
+        Application(Application&&) = delete;
         Application& operator=(const Application&) = delete;
         Application& operator=(Application&&) = delete;
 
-        virtual int start(unsigned int windowWidth, unsigned int windowHeight, const char* title);
+        virtual int start(unsigned int window_width, unsigned int window_height, const char* title);
 
-        virtual void on_update();
+        virtual void on_update() {}
+
+        virtual void on_ui_draw() {}
+
+        float camera_position[3] = { 0.f, 0.f, 1.f };
+        float camera_rotation[3] = { 0.f, 0.f, 0.f };
+        bool perspective_camera = false;
+        Camera camera;
+
+
     private:
-        std::unique_ptr<Window> window;
+        std::unique_ptr<class Window> m_pWindow;
 
-        EventDispatcher event_dispatcher;
-        bool closedWindow = false;
+        EventDispatcher m_event_dispatcher;
+        bool m_bCloseWindow = false;
     };
 
 }
--- SimpleEngineEditor/src/main.cpp
+++ SimpleEngineEditor/src/main.cpp
@@ -1,16 +1,29 @@
 #include <iostream>
 #include <memory>
+
+#include <imgui/imgui.h>
+
 #include <SimpleEngineCore/Application.hpp>
 
-class MyApp : public SimpleEngine::Application{
-    void on_update() override{
-        //std::cout << "Update frame: " << frame++ << std::endl;
-    }
-    int frame = 0;
+class SimpleEngineEditor : public SimpleEngine::Application {
+	void on_update() override {
+		//std::cout << "Update frame: " << frame++ << std::endl;
+	}
+
+	virtual void on_ui_draw() override
+	{
+		ImGui::Begin("Editor");
+		ImGui::SliderFloat3("camera position", camera_position, -10.f, 10.f);
+		ImGui::SliderFloat3("camera rotation", camera_rotation, 0, 360.f);
+		ImGui::Checkbox("Perspective camera", &perspective_camera);
+		ImGui::End();
+	}
+
+	int frame = 0;
 };
 
-int main(){
-    auto myApp = std::make_unique<MyApp>();
-    int retCode = myApp->start(1024, 768, "My application");
-    return retCode;
+int main() {
+	auto pSimpleEngineEditor = std::make_unique<SimpleEngineEditor>();
+	int returnCode = pSimpleEngineEditor->start(1024, 768, "SimpleEngine Editor");
+	return returnCode;
 }
